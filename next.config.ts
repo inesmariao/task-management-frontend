27d8b7232cfcbc226fabde9e03f0d60e38811cd4
inesmariao <63691309+inesmariao@users.noreply.github.com>
2024-12-14 00:28:09 -0500

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'standalone',
   env: {
@@ -7,4 +8,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
